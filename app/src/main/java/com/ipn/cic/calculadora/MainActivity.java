cc71c9ca0f28f8435436c44dd425672b3d6c7f29
FernandaCG <fi.camposg@gmail.com>
2018-03-10 20:33:36 -0600

    creacion proyecto, layers

---
+++ app/src/main/java/com/ipn/cic/calculadora/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ipn.cic.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.javier1nc.calculadora.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
